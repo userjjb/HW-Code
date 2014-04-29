3c975ef037d4b54d8f548690a695fdfb50504f55
Josh Bevan <userjjb@gmail.com>
2014-04-29 06:55:49 -0400

    Added circular conduction problem

---
+++ Multiscale CFD - UMLME/CircularRing.m
@@ -0,0 +1,16 @@
+%Josh Bevan 2014, 22.559 MS-CFD
+%Solves a 1D steady-state conduction problem with periodic boundary
+%conditions with heat generation 
+
+%________Input Params_______________________
+Bi= 0.05;   %Biot number for system
+L= 20;      %Length of rod in diameters
+xH= 3.5;    %Location of start of heated section, use n+1/2 to line up CV
+LH= 2;      %Length of heated section
+CV= 20;     %Number of CVs to use
+d= (L/CV)^2;%Grid size
+
+%The primarily tridiagonal structure is a simple Toeplitz matrix; Including
+%the same node twice in the domain necessitates a modification so the 2nd
+%derivative jumps the coincident node
+Theta = (toeplitz([-2/d-4*Bi, 1/d, 2:CV<0])+fliplr(blkdiag([0,d],zeros(CV-1,CV-3),[d,0]))) \ -and(0:CV>xH,0:CV<xH+LH)'
