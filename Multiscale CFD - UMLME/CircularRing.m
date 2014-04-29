67289bd98ceb0cdbec33c3b7b7126546a3c88135
Josh Bevan <userjjb@gmail.com>
2014-04-29 07:25:26 -0400

    Bug fixes and plotting
    Fixed bug with calculated coincident node jump entry

--- Multiscale CFD - UMLME/CircularRing.m
+++ Multiscale CFD - UMLME/CircularRing.m
@@ -8,9 +8,12 @@ L= 20;      %Length of rod in diameters
 xH= 3.5;    %Location of start of heated section, use n+1/2 to line up CV
 LH= 2;      %Length of heated section
 CV= 20;     %Number of CVs to use
-d= (L/CV)^2;%Grid size
+d= 1/(L/CV)^2;%Grid size
 
 %The primarily tridiagonal structure is a simple Toeplitz matrix; Including
 %the same node twice in the domain necessitates a modification so the 2nd
 %derivative jumps the coincident node
-Theta = (toeplitz([-2/d-4*Bi, 1/d, 2:CV<0])+fliplr(blkdiag([0,d],zeros(CV-1,CV-3),[d,0]))) \ -and(0:CV>xH,0:CV<xH+LH)'
+Theta = (toeplitz([-2*d-4*Bi, d, 2:CV<0])+fliplr(blkdiag([0,d],zeros(CV-1,CV-3),[d,0]))) \ -and(0:L/CV:L>xH,0:L/CV:L<xH+LH)'
+
+
+plot(0:L/CV:L,Theta), xlabel('Axial Location (x/D)'), ylabel('Non-dimensional Temperature (theta)')
